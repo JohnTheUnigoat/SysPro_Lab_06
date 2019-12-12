7e5d2d51b9c0ff8174da1fa5bcea246b88181c08
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-12 13:17:20 +0200

    Created ClientInfo class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_06/Classes/ClientInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_06
+{
+    class ClientInfo
+    {
+        public DateTime ArrivalDate { get; }
+        public DateTime DepartureDate { get; }
+        public int RoomIndex { get; }
+
+        public ClientInfo(DateTime arrivalDate, int daysOfStay, int roomIndex)
+        {
+            ArrivalDate = arrivalDate;
+            DepartureDate = arrivalDate.AddDays(daysOfStay);
+            RoomIndex = roomIndex;
+        }
+    }
+}
