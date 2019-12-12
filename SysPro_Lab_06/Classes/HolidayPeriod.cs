96aca9cc08e8387ae0086c7ef0106a202cce9587
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-12 13:51:18 +0200

    Created HolidayPeriod class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_06/Classes/HolidayPeriod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_06
+{
+    class HolidayPeriod
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+
+        private List<ClientInfo> clients;
+        public ReadOnlyCollection<ClientInfo> Clients
+        {
+            get
+            {
+                return new ReadOnlyCollection<ClientInfo>(clients);
+            }
+        }
+
+        public HolidayPeriod(DateTime startDate, int durationDays)
+        {
+            StartDate = startDate;
+            EndDate = startDate.AddDays(durationDays);
+
+            clients = new List<ClientInfo>();
+        }
+
+        public bool TryAddClient(DateTime arrivalDate, int daysOfStay, int roomIndex)
+        {
+            if(arrivalDate < StartDate || arrivalDate >= EndDate)
+                return false;
+
+            if (arrivalDate.AddDays(daysOfStay) >= EndDate)
+                return false;
+
+            clients.Add(new ClientInfo(arrivalDate, daysOfStay, roomIndex));
+
+            return true;
+        }
+    }
+}
