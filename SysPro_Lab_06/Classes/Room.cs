24225d4e5e11ddb77c88b75feb3d240387b16360
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-12-12 13:12:53 +0200

    Created Room class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_06/Classes/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_06
+{
+    class Room
+    {
+        public int MaxNumberOfPeople { get; }
+        public double PricePerDay { get; set; }
+        public bool IsOccupied { get; set; }
+
+        public Room(int maxNumberOfPeople, double pricePerDay)
+        {
+            MaxNumberOfPeople = maxNumberOfPeople;
+            PricePerDay = pricePerDay;
+            IsOccupied = false;
+        }
+    }
+}
